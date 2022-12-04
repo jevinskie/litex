83417f68fb3b4c837e3d525eb7ae89aa0cb3688d
Jevin Sweval <jevinsweval@gmail.com>
2022-12-04 15:33:32 -0500

    run_remote works

--- litex/build/remote.py
+++ litex/build/remote.py
@@ -1,1 +1,35 @@
+import os
+
+import rpyc
+from rpyc.core.service import ClassicService
+from rpyc.utils.server import ThreadPoolServer
 import sysrsync
+import fabric
+
+def _getenv(var):
+    val = os.getenv(var)
+    if val is None:
+        raise KeyError(f"environment variable '{var}' is not defined")
+    return val
+
+def get_remote_host(host):
+    if host is not None:
+        return host
+    return _getenv("LITEX_REMOTE_BUILD_HOST")
+
+def get_remote_user(user):
+    if user is not None:
+        return user
+    return os.getenv("LITEX_REMOTE_BUILD_USER")
+
+def run_remote(host, args, user=None, pty=False):
+    host = get_remote_host(host)
+    user = get_remote_user(user)
+    conn = fabric.Connection(host, user)
+    term = os.getenv("TERM", "vt100")
+    conn.run(f"env TERM={term} " + " ".join(args), pty=pty)
+
+def run_build_server(host, user=None):
+    host = get_remote_host(host)
+    user = get_remote_user(user)
+    # run_remote(host, , user=None, pty=False)
--- litex/tools/litex_remote_build.py
+++ litex/tools/litex_remote_build.py
@@ -1,17 +1,24 @@
 import argparse
 
-from litex.build.remote import *
+from litex.build.remote import run_build_server, run_remote
 
 # Run ----------------------------------------------------------------------------------------------
 
 def _get_args():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("--run", nargs=argparse.REMAINDER, help="run command on remote node")
+    parser.add_argument("--pty", action="store_true", help="pty mode (act like a real terminal)")
+    parser.add_argument("--serve", action="store_true", help="run the LiteX build server")
+    parser.add_argument("--host", help="remote build server hostname")
+    parser.add_argument("--user", help="remote build server username")
     return parser.parse_args()
 
 def main():
     args = _get_args()
-
+    if args.run is not None:
+        run_remote(args.host, args.run, user=args.user, pty=args.pty)
+    elif args.serve:
+        run_build_server()
 
 if __name__ == "__main__":
     main()
