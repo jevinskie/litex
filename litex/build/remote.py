c43d991c54d619f872ee99437794eb5397c824d7
Jevin Sweval <jevinsweval@gmail.com>
2022-12-05 09:06:12 -0500

    pty improvements

--- litex/build/remote.py
+++ litex/build/remote.py
@@ -1,10 +1,12 @@
 import os
+import sys
 
+import fabric
 import rpyc
+import sysrsync
 from rpyc.core.service import ClassicService
 from rpyc.utils.server import ThreadPoolServer
-import sysrsync
-import fabric
+
 
 def _getenv(var):
     val = os.getenv(var)
@@ -22,14 +24,22 @@ def get_remote_user(user):
         return user
     return os.getenv("LITEX_REMOTE_BUILD_USER")
 
-def run_remote(host, args, user=None, pty=False):
+def run_remote(host, args, user=None, pty=False, **kwargs):
     host = get_remote_host(host)
     user = get_remote_user(user)
     conn = fabric.Connection(host, user)
-    term = os.getenv("TERM", "vt100")
-    conn.run(f"env TERM={term} " + " ".join(args), pty=pty)
+    kwargs.update(pty=pty, err_stream=sys.stderr, out_stream=sys.stdout)
+    conn.run(" ".join(args), **kwargs)
 
-def run_build_server(host, user=None):
+
+def run_build_server_remotely(host, user=None):
     host = get_remote_host(host)
     user = get_remote_user(user)
-    # run_remote(host, , user=None, pty=False)
+    conn = fabric.Connection(host, user)
+    run_kwargs = dict(pty=True, err_stream=sys.stderr, out_stream=sys.stdout)
+    prom = conn.run("sh -l -c 'litex_remote_build --serve'", asynchronous=True, **run_kwargs)
+    print("after run")
+    res = prom.join()
+
+def run_build_server():
+    print("serving")
